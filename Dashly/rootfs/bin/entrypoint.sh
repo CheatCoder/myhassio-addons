02a137f05d89c1259415ee5d30c8a7875c3d6f1b
CheatCoder <stefan.sauer@tutanota.com>
2023-08-05 13:11:44 +0200

    change something

--- Dashly/rootfs/bin/entrypoint.sh
+++ Dashly/rootfs/bin/entrypoint.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
+FILE=/data/config.yml
+if test -f "$FILE"; then
+    echo "$FILE exists."
+    ln -s /data/config.yml /app/dashly/public/config.yml
+else
+    cp /config.yml /data
+    ln -s /data/config.yml /app/dashly/public/config.yml
+fi
+
+cd /app/dashly
+
 yarn build-and-start
