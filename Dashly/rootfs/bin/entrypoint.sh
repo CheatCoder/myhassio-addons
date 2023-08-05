f4377a18664eabde173967cc3ab8ca9b0266b19d
sauer23@duck.com <sauer23@duck.com>
2023-08-05 20:57:39 +0200

    fixed typo

--- Dashly/rootfs/bin/entrypoint.sh
+++ Dashly/rootfs/bin/entrypoint.sh
@@ -2,17 +2,17 @@
 
 #rm /app/dashly/public/conf.yml
 
-mkdir -p /config/addons_config/dashly
-FILE=/config/addons_config/dashly/conf.yml
+mkdir -p /config/addons_config/dashy
+FILE=/config/addons_config/dashy/conf.yml
 
 if test -f "$FILE"; then
     echo "$FILE exists."
-    ln -s $FILE /app/dashly/public/conf.yml
+    ln -s $FILE /app/dashy/public/conf.yml
 else
     cp /conf.yml $FILE
-    ln -s $FILE /app/dashly/public/conf.yml
+    ln -s $FILE /app/dashy/public/conf.yml
 fi
 
-cd /app/dashly
+cd /app/dashy
 
 yarn start
