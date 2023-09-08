678aaa2061bb7dfbc8c03e0bc9d47408dc58f970
Stefan Sauer <stefan.sauer@tutanota.com>
2023-09-08 16:38:23 +0200

    Benotes test

---
+++ Benotes/rootfs/bin/start.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+#/var/www/storage/app/public/thumbnails
+rm -rf /var/www/storage/app/public/thumbnails
+mkdir -p /data/thumbnails
+ln -s /data/thumbnails /var/www/storage/app/public/thumbnails
+
+#/var/www/storage
+rm -rf /var/www/storage
+mkdir -p /data/storage
+ln -s /data/storage /var/www/storage
