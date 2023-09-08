94b9f4f1d051642d3915dfbe3ba85a0218438772
Stefan Sauer <stefan.sauer@tutanota.com>
2023-09-08 18:08:45 +0200

    change something

--- Benotes/rootfs/bin/start.sh
+++ Benotes/rootfs/bin/start.sh
@@ -2,12 +2,12 @@
 
 #/var/www/storage/app/public/thumbnails
 rm -rf /var/www/storage/app/public/thumbnails
-mkdir -p /share/benotes/thumbnails 2>/dev/null
+mkdir -p /share/benotes/thumbnails
 ln -s /share/benotes/thumbnails /var/www/storage/app/public/thumbnails
 
 #/var/www/storage
 rm -rf /var/www/storage
-mkdir -p /share/benotes/storage 2>/dev/null
+mkdir -p /share/benotes/storage
 ln -s /share/benotes/storage /var/www/storage
 
 
